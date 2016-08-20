05656455bed5cce54398b326814a44e451df515f
LycheeLisa <lisajia.chen@mail.utoronto.ca>
2016-08-20 16:43:02 -0400

    Added navigation bar

--- challengeme.js
+++ challengeme.js
@@ -1,3 +1,9 @@
-/**
- * Created by chenji13 on 8/20/2016.
- */
+/* Navigation Bar Stuff */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
